a7dc744bdd0669a2f85d42e36d6c6584c7c073bf
isidoris <valentin.sorlin@hotmail.com>
2017-09-25 12:38:32 +0200

    langue and rand number

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends Controller
+{
+    // [...]
+    
+    /**
+     * @Route("/blog/{year}/{title}/{_locale}",
+     *     defaults={"_locale": "en"}, requirements={
+     *         "year": "\d{4}",
+     *         "title": "^[a-zA-Z0-9-]+$",
+     *         "_locale": "en|fr"
+     *     })
+     */
+    public function numberAction($_locale, $year, $title)
+    {
+        if ($_locale == 'fr') {
+            return new Response("Voici l'article $title, de l'année $year, en langue $_locale");
+        } else if ($_locale == 'en') {
+            return new Response("there is the article $title, from year $year, in $_locale");
+        } else if ($_locale == 'ru') {
+            return new Response("Cyka Blyat!");            
+        }
+    }
+}
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,30 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
+    // [...]
+    
     /**
-     * @Route("/", name="homepage")
+     * @Route("/lucky/number/{min}/{max}", name="lucky_number", requirements={"min": "\d+", "min", "\d+"})
      */
-    public function indexAction(Request $request)
+    public function numberAction($min, $max)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        if ($min <= $max) {
+            $number = mt_rand($min, $max);
+            //ici on va chercher le template et on lui transmet la variable
+            return $this->render('AppBundle:Default:number.html.twig', array(
+                // pour fournir des variables au template
+                // a gauche, le nom qui sera utilisé dans le template
+                // a droite, la valeur
+                'number' => $number
+            ));    
+        } else {
+            return new Response(
+                'la valeur min doit être inferieur a la valeur max'
+            );
+        }
     }
 }
