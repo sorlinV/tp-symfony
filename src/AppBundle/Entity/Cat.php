9a92e14721d8372d2f8d065243b3ba6f12d68d40
isidoris <valentin.sorlin@hotmail.com>
2017-09-26 12:05:36 +0200

    adding cat

---
+++ src/AppBundle/Entity/Cat.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cat
+ *
+ * @ORM\Table(name="cat")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CatRepository")
+ */
+class Cat
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="master", type="string", length=255)
+     */
+    private $master;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="mouseKilled", type="integer")
+     */
+    private $mouseKilled;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Cat
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Cat
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Cat
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set master
+     *
+     * @param string $master
+     *
+     * @return Cat
+     */
+    public function setMaster($master)
+    {
+        $this->master = $master;
+
+        return $this;
+    }
+
+    /**
+     * Get master
+     *
+     * @return string
+     */
+    public function getMaster()
+    {
+        return $this->master;
+    }
+
+    /**
+     * Set mouseKilled
+     *
+     * @param integer $mouseKilled
+     *
+     * @return Cat
+     */
+    public function setMouseKilled($mouseKilled)
+    {
+        $this->mouseKilled = $mouseKilled;
+
+        return $this;
+    }
+
+    /**
+     * Get mouseKilled
+     *
+     * @return int
+     */
+    public function getMouseKilled()
+    {
+        return $this->mouseKilled;
+    }
+}
+
